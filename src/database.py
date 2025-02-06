2b8d36ccf4c79aac73af261db680d6ef799cc69e
leo <elkinlrc@gmail.com>
2025-02-06 12:32:52 +0100

    ✅ Conexión a PostgreSQL establecida y probada con éxito, se creo crud y modelos

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://rust_user:Aust6canc@localhost/syncHero"
+DATABASE_URL = "postgresql://rust_user:123456@localhost/syncHero"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
